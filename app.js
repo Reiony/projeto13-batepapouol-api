530a5c62ab3ca7443c86029669ec5d0d3efe044c
Rennan <reiony.dev@gmail.com>
2022-11-14 15:09:34 -0300

    finished post and get from participants route

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-try{
+try {
     await mongoClient.connect()
     db = mongoClient.db("batepapoUol");
-} catch (err){
+} catch (err) {
     console.log(err);
 }
 
@@ -34,5 +34,50 @@ const userSchema = joi.object({
     name: joi.string().min(1).required()
 })
 
+//Routes
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    const validation = userSchema.validate(name, { abortEarly: false });
+    if (validation.error) {
+        const error = validation.error.details.map(error => error.message);
+        res.status(422).send(error);
+        return
+    }
+    const VerifyUser = participantsColl.findOne({ name: name })
+    if (VerifyUser) {
+        res.sendStatus(409)
+        return
+    }
+
+    try {
+        await participantsColl.insertOne({ name: name, lastStatus: Date.now() })
+        await messagesColl.insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs.format('HH:MM:SS') })
+        res.sendStatus(201)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+app.get("/participants", async (req, res) => {
+    try {
+        const participantsdata = await participantsColl.find.toArray()
+        res.status(201).send(participantsdata)
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
+app.post("/messages", async (req, res) => {
+
+})
+
+app.get("/messages", async (req, res) => {
+
+})
+
+app.post("/status", async (req, res) => {
+
+})
 
 app.listen(5000, () => console.log('Server running on port 5000'))
